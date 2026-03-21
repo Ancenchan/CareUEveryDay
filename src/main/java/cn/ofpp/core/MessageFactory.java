0a57f70013edc59367606fc11fbcd43b7432b9c6
Ancen <ancen2@163.com>
2026-03-22 01:53:24 +0800

    Update MessageFactory.java

--- src/main/java/cn/ofpp/core/MessageFactory.java
+++ src/main/java/cn/ofpp/core/MessageFactory.java
@@ -15,6 +15,9 @@ public class MessageFactory {
         // 2. 获取诗词数据
         RandomAncientPoetry.AncientPoetry poetry = RandomAncientPoetry.getNext();
         
+        // 3. 从环境变量读取模板 ID (这样就不依赖 Bootstrap 类了)
+        String templateId = cn.hutool.core.util.StrUtil.blankToDefault(System.getenv("TEMPLATE_ID"), "");
+        
         List<WxMpTemplateData> data = new ArrayList<>();
         // 基础信息
         data.add(new WxMpTemplateData("friendName", "亲爱的老妈", "#FF80AB"));
@@ -25,17 +28,17 @@ public class MessageFactory {
         data.add(new WxMpTemplateData("tempRange", weather.getNighttemp() + "℃ ~ " + weather.getDaytemp() + "℃", "#FFA726"));
         data.add(new WxMpTemplateData("tips", weather.getTips(), "#9575CD"));
         
-        // 生日倒计时
+        // 生日倒数
         data.add(new WxMpTemplateData("nextBirthday", friend.getNextBirthdayDays(), "#EF5350"));
 
-        // --- 重点：诗词字段（必须和后台模板 {{XXX.DATA}} 一一对应） ---
+        // 诗词字段
         data.add(new WxMpTemplateData("content", poetry.getContent(), "#7E57C2"));
         data.add(new WxMpTemplateData("author", poetry.getAuthor(), "#9E9E9E"));
         data.add(new WxMpTemplateData("origin", "《" + poetry.getOrigin() + "》", "#9E9E9E"));
         
         return WxMpTemplateMessage.builder()
                 .toUser(friend.getUserId())
-                .templateId(Bootstrap.TEMPLATE_ID)
+                .templateId(templateId)
                 .data(data)
                 .build();
     }
