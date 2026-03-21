6bd6d7cbf56fd20fc64eb4fc8110fc5b15310d0d
Ancen <ancen2@163.com>
2026-03-22 01:30:22 +0800

    Update WeatherInfo.java

--- src/main/java/cn/ofpp/core/WeatherInfo.java
+++ src/main/java/cn/ofpp/core/WeatherInfo.java
@@ -1,72 +1,25 @@
 package cn.ofpp.core;
 
-/**
- * @author DokiYolo
- * Date 2022-08-22
- */
-public class WeatherInfo {
+import lombok.Data;
 
+/**
+ * 对应高德 API 的天气信息模型
+ * 使用 @Data 注解自动生成 Getter/Setter
+ */
+@Data
+public class WeatherInfo {
     private String province;
     private String city;
+    private String adcode;
     private String weather;
-    private String temperature;
+    private String temperature; // 实时温度（base模式用）
     private String winddirection;
     private String windpower;
     private String humidity;
+    private String reporttime;
 
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getWeather() {
-        return weather;
-    }
-
-    public void setWeather(String weather) {
-        this.weather = weather;
-    }
-
-    public String getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(String temperature) {
-        this.temperature = temperature;
-    }
-
-    public String getWinddirection() {
-        return winddirection;
-    }
-
-    public void setWinddirection(String winddirection) {
-        this.winddirection = winddirection;
-    }
-
-    public String getWindpower() {
-        return windpower;
-    }
-
-    public void setWindpower(String windpower) {
-        this.windpower = windpower;
-    }
-
-    public String getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(String humidity) {
-        this.humidity = humidity;
-    }
+    // --- 重点新增字段 ---
+    private String daytemp;      // 白天最高温
+    private String nighttemp;    // 晚上最低温
+    private String tips;         // 穿衣建议（我们在 GaodeUtil 里生成的）
 }
