28bfe899e7336638ee2561993fd3ee408e828ba5
Ancen <ancen2@163.com>
2026-03-22 01:42:36 +0800

    Update MessageFactory.java

--- src/main/java/cn/ofpp/core/MessageFactory.java
+++ src/main/java/cn/ofpp/core/MessageFactory.java
@@ -1,55 +1,34 @@
 package cn.ofpp.core;
 
-import cn.hutool.core.util.StrUtil;
-import cn.ofpp.Bootstrap;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MessageFactory {
 
     public static WxMpTemplateMessage resolveMessage(Friend friend) {
-        return WxMpTemplateMessage.builder()
-                .toUser(friend.getUserId())
-                .templateId(StrUtil.emptyToDefault(friend.getTemplateId(), Bootstrap.TEMPLATE_ID))
-                .data(buildData(friend))
-                .build();
-    }
-
-    private static List<WxMpTemplateData> buildData(Friend friend) {
+        // 调用 GaodeUtil 获取数据
         String adcCode = GaodeUtil.getAdcCode(friend.getProvince(), friend.getCity());
         WeatherInfo weather = GaodeUtil.getForecastWeatherInfo(adcCode);
-        RandomAncientPoetry.AncientPoetry ancientPoetry = RandomAncientPoetry.getNext();
-
-        return List.of(
-                TemplateDataBuilder.builder().name("friendName").value("亲爱的老妈").color("#FF80AB").build(),
-                TemplateDataBuilder.builder().name("city").value(friend.getCity()).color("#4FC3F7").build(),
-                TemplateDataBuilder.builder().name("weather").value(weather.getWeather()).color("#66BB6A").build(),
-                TemplateDataBuilder.builder().name("tempRange").value(weather.getNighttemp() + "℃ ~ " + weather.getDaytemp() + "℃").color("#FFA726").build(),
-                TemplateDataBuilder.builder().name("tips").value(weather.getTips()).color("#9575CD").build(),
-                TemplateDataBuilder.builder().name("nextBirthday").value(friend.getNextBirthdayDays()).color("#EF5350").build(),
-                TemplateDataBuilder.builder().name("author").value(ancientPoetry.getAuthor()).color("#F53F3F").build(),
-                TemplateDataBuilder.builder().name("origin").value(ancientPoetry.getOrigin()).color("#F53F3F").build(),
-                TemplateDataBuilder.builder().name("content").value(ancientPoetry.getContent()).color("#F53F3F").build()
-        );
-    }
-
-    // --- 这个内部类一定要保留在文件里 ---
-    static class TemplateDataBuilder {
-        private String name;
-        private String value;
-        private String color;
-
-        public static TemplateDataBuilder builder() { return new TemplateDataBuilder(); }
-        public TemplateDataBuilder name(String name) { this.name = name; return this; }
-        public TemplateDataBuilder value(String value) { this.value = value; return this; }
-        public TemplateDataBuilder color(String color) { this.color = color; return this; }
-        public WxMpTemplateData build() {
-            WxMpTemplateData data = new WxMpTemplateData();
-            data.setName(name);
-            data.setValue(value);
-            data.setColor(color);
-            return data;
-        }
+        
+        List<WxMpTemplateData> data = new ArrayList<>();
+        data.add(new WxMpTemplateData("friendName", "亲爱的老妈", "#FF80AB"));
+        data.add(new WxMpTemplateData("city", friend.getCity(), "#4FC3F7"));
+        data.add(new WxMpTemplateData("weather", weather.getWeather(), "#66BB6A"));
+        
+        // 气温区间
+        data.add(new WxMpTemplateData("tempRange", weather.getNighttemp() + "℃ ~ " + weather.getDaytemp() + "℃", "#FFA726"));
+        // 穿衣建议
+        data.add(new WxMpTemplateData("tips", weather.getTips(), "#9575CD"));
+        
+        // 生日倒计时（假设你的 Friend 类有这个方法）
+        data.add(new WxMpTemplateData("nextBirthday", friend.getNextBirthdayDays(), "#EF5350"));
+        
+        return WxMpTemplateMessage.builder()
+                .toUser(friend.getUserId())
+                .templateId(cn.ofpp.Bootstrap.TEMPLATE_ID)
+                .data(data)
+                .build();
     }
 }
