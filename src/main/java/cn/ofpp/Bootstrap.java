a5a058f763add8950b31d7a2bde214955c80591e
Ancen <ancen2@163.com>
2026-03-22 01:05:26 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -14,17 +14,17 @@ public class Bootstrap {
     /**
      * 公众号AppID
      */
-    public static final String APP_ID = "";
+    public static final String APP_ID = "wxcaaf1c090835d613";
 
     /**
      * 公众号秘钥
      */
-    public static final String SECRET = "";
+    public static final String SECRET = "40021554acb172c48b8c317dbe58f9c9";
 
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "";
+    public static final String TEMPLATE_ID = "39J668qcdLTg446XjbqR1OPkYElxfqEaWtVKZw8MbKw";
 
     /**
      * 初始化
