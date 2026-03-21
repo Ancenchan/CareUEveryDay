8cabc135451e063eac7b4861e5a259ab338c5f7c
Ancen <ancen2@163.com>
2026-03-22 01:55:25 +0800

    Update MessageFactory.java

--- src/main/java/cn/ofpp/core/MessageFactory.java
+++ src/main/java/cn/ofpp/core/MessageFactory.java
@@ -4,31 +4,27 @@ import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
 import java.util.ArrayList;
 import java.util.List;
+import cn.ofpp.Bootstrap; // 必须导入这个，否则编译报错
 
 public class MessageFactory {
 
     public static WxMpTemplateMessage resolveMessage(Friend friend) {
-        // 1. 获取天气数据
+        // 1. 获取天气和诗词
         String adcCode = GaodeUtil.getAdcCode(friend.getProvince(), friend.getCity());
         WeatherInfo weather = GaodeUtil.getForecastWeatherInfo(adcCode);
-        
-        // 2. 获取诗词数据
         RandomAncientPoetry.AncientPoetry poetry = RandomAncientPoetry.getNext();
         
-        // 3. 从环境变量读取模板 ID (这样就不依赖 Bootstrap 类了)
-        String templateId = cn.hutool.core.util.StrUtil.blankToDefault(System.getenv("TEMPLATE_ID"), "");
+        // 2. 这里的读取方式改回你之前成功的样子 (通过 Bootstrap.TEMPLATE_ID)
+        String templateId = cn.hutool.core.util.StrUtil.blankToDefault(friend.getTemplateId(), Bootstrap.TEMPLATE_ID);
         
         List<WxMpTemplateData> data = new ArrayList<>();
-        // 基础信息
         data.add(new WxMpTemplateData("friendName", "亲爱的老妈", "#FF80AB"));
         data.add(new WxMpTemplateData("city", friend.getCity(), "#4FC3F7"));
         data.add(new WxMpTemplateData("weather", weather.getWeather(), "#66BB6A"));
         
-        // 气温与建议
+        // 新加的字段
         data.add(new WxMpTemplateData("tempRange", weather.getNighttemp() + "℃ ~ " + weather.getDaytemp() + "℃", "#FFA726"));
         data.add(new WxMpTemplateData("tips", weather.getTips(), "#9575CD"));
-        
-        // 生日倒数
         data.add(new WxMpTemplateData("nextBirthday", friend.getNextBirthdayDays(), "#EF5350"));
 
         // 诗词字段
