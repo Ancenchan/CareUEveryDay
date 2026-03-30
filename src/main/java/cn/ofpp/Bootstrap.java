7e98118ccda110e7e163b22da8ed48752d756adc
Ancen <ancen2@163.com>
2026-03-31 00:43:11 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -24,7 +24,7 @@ public class Bootstrap {
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "DNXhdWgbeTwafjIY4pHjmVTAY2aNPq7reORyOxUmxs0";
+    public static final String TEMPLATE_ID = "e0629CvSKT1kujR9svvskYK-jA-Kp7qoe1d3U-9Dknk";
 
     /**
      * 初始化
